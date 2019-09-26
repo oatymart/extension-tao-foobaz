b600465ae9fa57d0b1880f83e3ef1d27135fb4b4
Martin Nicholson <martin@taotesting.com>
2019-09-26 10:49:08 +0200

    Reset to version 1.4.8

--- manifest.php
+++ manifest.php
@@ -26,7 +26,7 @@ return [
     'label' => 'FooBar power extension ok',
     'description' => 'Dummy test extension',
     'license' => 'GPL-2.0',
-    'version' => '1.4.9',
+    'version' => '1.4.8',
     'author' => 'Open Assessment Technologies, CRP Henri Tudor',
     'requires' => [
         'tao' => '>=21.0.1'
