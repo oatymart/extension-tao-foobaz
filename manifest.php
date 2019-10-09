1525cf53ff65a081f045fa8fec2ea6d509a5b378
Martin Nicholson <martin@taotesting.com>
2019-10-09 12:27:19 +0200

    Bump to 1.4.20 on develop

--- manifest.php
+++ manifest.php
@@ -26,7 +26,7 @@ return [
     'label' => 'FooBar power extension yes',
     'description' => 'Dummy test extension',
     'license' => 'GPL-2.0',
-    'version' => '1.4.19',
+    'version' => '1.4.20',
     'author' => 'Open Assessment Technologies, CRP Henri Tudor',
     'requires' => [
         'tao' => '>=21.0.1'
